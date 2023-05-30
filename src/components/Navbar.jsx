5088b8be2b1e1129387ef28a25577bf59010ed3a
Japmann <kaurbanga30@yahoo.com>
2023-05-31 03:23:27 +0530

    PageChanges2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,10 @@
-import {Link} from 'react-router-dom'
-import '../css/Navbar.css'
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import '../css/Navbar.css';
+
 const Navbar = () => {
+  const location = useLocation();
+
   return (
     <div className='nav-parent'>
       <div className="logo">
@@ -9,22 +13,33 @@ const Navbar = () => {
       </div>
       <div className="navlinks">
         <li>
-          <Link to='/'>Home</Link>
+          <Link to='/' className={location.pathname === '/' ? 'active' : ''}>
+            Home
+          </Link>
         </li>
         <li>
-          <Link to='/about'>About</Link>
+          <Link to='/about' className={location.pathname === '/about' ? 'active' : ''}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to='/projects'>Projects</Link>
+          <Link to='/projects' className={location.pathname === '/projects' ? 'active' : ''}>
+            Projects
+          </Link>
         </li>
         <li>
-          <Link to='/blogs'>Blogs</Link>
+          <Link to='/blogs' className={location.pathname === '/blogs' ? 'active' : ''}>
+            Blogs
+          </Link>
         </li>
         <li>
-          <Link to='/contact'>Contact</Link> 
+          <Link to='/contact' className={location.pathname === '/contact' ? 'active' : ''}>
+            Contact
+          </Link> 
         </li>
       </div>
     </div>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,11 +1,103 @@
 import Navbar from '../components/Navbar'
 import Background from '../components/Background'
+import '../css/Blogs.css'
+import '../css/Scrollbar.css';
+import UI from '../images/UI.jpg'
+
+
 const Blogs = () => {
+
+  const handleDivClick = () => {
+    // Logic to execute when the div is clicked
+    // For example, you can use the location.href to navigate to a specific URL
+    window.location.href = 'https://japmann.hashnode.dev/journey-to-becoming-a-uiux-designer';
+  };
+
   return (
     <div>
       <Navbar/>
       <Background/>
-      Blogs
+      <div className='cont'>
+        <div className='title'>All Posts</div>
+        <div className='line'></div>
+      </div>
+      <div className='articles' style={{ overflow: 'auto' }}>
+        <div className='my-blog' onClick={handleDivClick}>
+          <div className='blog-text'>
+            <h4>Journey to becoming a UI/UX Designer</h4>
+            <div className='read'>
+              <div className='dates'>
+                Jan 6, 2023 |
+              </div>
+              <div className='book'>
+                <i class="fas fa-book-open"></i>
+                5 min read
+              </div>
+            </div>
+            <div className='tags'>
+              <span>#UX/UI Design  </span>
+              <span>#User Research  </span>
+              <span>#User Experience  </span>
+            </div>
+            <div className='in-txt'>
+            User interface (UI) design is the process designers use to create interfaces in software or computerized devices emphasizing appearance and style. Designers aim to create user-friendly and enjoyable interfaces. UI design refers to graphical user interfaces and other forms such as Voice control interfaces.
+            </div>
+          </div>
+          <div className='blogimg'>
+            <img src={UI}/>
+          </div>
+        </div>
+        <div className='my-blog'>
+          <div className='blog-text'>
+            <h4>Journey to becoming a UI/UX Designer</h4>
+            <div className='read'>
+              <div className='dates'>
+                Jan 6, 2023 |
+              </div>
+              <div className='book'>
+                <i class="fas fa-book-open"></i>
+                5 min read
+              </div>
+            </div>
+            <div className='tags'>
+              <span>#UX/UI Design</span>
+              <span>#User Research</span>
+              <span>#User Experience</span>
+            </div>
+            <div className='in-txt'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti maiores, sapiente qui doloremque totam officia? Ex aliquam aperiam quam saepe! Ea, magni suscipit. Assumenda amet ea laboriosam. Nisi, ea dignissimos?
+            </div>
+          </div>
+          <div className='blogimg'>
+            <img src={UI}/>
+          </div>
+        </div>
+        <div className='my-blog'>
+          <div className='blog-text'>
+            <h4>Journey to becoming a UI/UX Designer</h4>
+            <div className='read'>
+              <div className='dates'>
+                Jan 6, 2023 |
+              </div>
+              <div className='book'>
+                <i class="fas fa-book-open"></i>
+                5 min read
+              </div>
+            </div>
+            <div className='tags'>
+              <span>#UX/UI Design</span>
+              <span>#User Research</span>
+              <span>#User Experience</span>
+            </div>
+            <div className='in-txt'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti maiores, sapiente qui doloremque totam officia? Ex aliquam aperiam quam saepe! Ea, magni suscipit. Assumenda amet ea laboriosam. Nisi, ea dignissimos?
+            </div>
+          </div>
+          <div className='blogimg'>
+          <img src={UI}/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
