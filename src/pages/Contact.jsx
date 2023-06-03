5ba0e876cc73dc620e94e3688419cbed872121ce
Japmann <kaurbanga30@yahoo.com>
2023-06-03 13:29:48 +0530

    PageChanges6

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,41 @@
 import Navbar from '../components/Navbar'
 import Background from '../components/Background'
+import '../css/Contact.css'
+
 const Contact = () => {
   return (
     <div>
-      <Navbar/>
-      <Background/>
-      Contact
+      <Navbar />
+      <Background />
+      <div className='contact-page'>
+        <div className='contact-top'>
+          <div className='contact-text'>
+            <p>Let's Connect!!!</p>
+            <span>Feel free to drop any suggestions or feedback. Besides, I am also ready to join a company to contribute to the projects and put forth the best of my potential.</span>
+          </div>
+          <div className='contact-msg'>
+            <input className='send-name' placeholder='Your Name' type='text'></input>
+            <input className='send-mail' placeholder='Your Email' type='mail'></input>
+            <input className='send-msg' placeholder='Your Message' type='text'></input>
+            <button className='send-btn'>Send 📩</button>
+          </div>
+          <div className='contact-tag'>
+            <div className='dark-tag'>CONTACT</div>
+            <div className='tag-line'></div>
+          </div>
+        </div>
+        <div className='contact-bottom'>
+          <div className='devs'>
+
+          </div>
+          <div className='thanks'>
+
+          </div>
+          <div className='social-pros'>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
