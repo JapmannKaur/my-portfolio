222b8d96667a814898eab689c270c01a5a7768f5
Japmann <kaurbanga30@yahoo.com>
2023-06-05 03:13:26 +0530

    PageChanges8

--- src/data.jsx
+++ src/data.jsx
@@ -1,5 +1,23 @@
+import hotstar from '../src/images/hotstar.jpg'
+import task from '../src/images/task.jpg'
+import search from '../src/images/search.jpg'
+import snack from '../src/images/snack.jpg'
+import road from '../src/images/road.jpg'
+import smoke from '../src/images/smoke.jpg'
+
 export const socialLinks = [
     {id:1,href: 'https://www.linkedin.com/in/japmann-kaur-banga-43962b1bb/',icon:'fab fa-linkedin-in'}, 
     {id:2,href: 'https://www.instagram.com/japmann3003/',icon:'fab fa-instagram'},
     {id:3,href: 'mailto:kaurbanga30@yahoo.com',icon:'fa fa-envelope'},
+];
+
+export const projdata = [
+    {id:1, image:hotstar, ptitle:'Hotstar Clone',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:2, image:task, ptitle:'Task Organizer',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:3, image:search, ptitle:'Food Search Engine',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:4, image:hotstar, ptitle:'React App',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:5, image:snack, ptitle:'SnackBite UI',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:6, image:road, ptitle:'Road Accidents Data Analysis',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:7, image:smoke, ptitle:'Wildfire Detection using CNN',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
+    {id:8, image:hotstar, ptitle:'Secure Organ Transplation',tech:'#HTML, CSS, JavaScript', deploy:'https://hotstar-clone-flax.vercel.app/'},
 ];
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,12 +1,84 @@
-import Navbar from '../components/Navbar'
-import Background from '../components/Background'
+import React, { useState } from 'react';
+
+import Navbar from '../components/Navbar';
+import Background from '../components/Background';
+import { projdata } from '../data';
+import '../css/Projects.css';
+
 const Projects = () => {
+  const [selectedProject, setSelectedProject] = useState(null);
+  const [hoveredProjectId, setHoveredProjectId] = useState(null);
+
+  const handleDetailsClick = (project) => {
+    setSelectedProject(project);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedProject(null);
+  };
+
+  const handleMouseEnter = (id) => {
+    setHoveredProjectId(id);
+  };
+
+  const handleMouseLeave = () => {
+    setHoveredProjectId(null);
+  };
+
   return (
     <div>
-      <Navbar/>
-      <Background/>
-      Projects
+      <Navbar />
+      <Background />
+      <div className='my-projects'>
+        My Recent <div className='only-proj'>Projects</div>
+      </div>
+      <div className='all-projects'>
+        {projdata.map((pdata) => {
+          const { id, image, ptitle, tech, deploy } = pdata;
+
+          return (
+            <div
+              className='myp-data'
+              key={id}
+              onMouseEnter={() => handleMouseEnter(id)}
+              onMouseLeave={handleMouseLeave}
+            >
+              <img
+                src={image}
+                className={`projimage ${
+                  hoveredProjectId === id ? 'projimage-hover' : ''
+                }`}
+              />
+              <h3 className='projtitle'>{ptitle}</h3>
+              {hoveredProjectId === id && (
+                <button
+                  className='details-button'
+                  onClick={() => handleDetailsClick(pdata)}
+                >
+                  Details
+                </button>
+              )}
+            </div>
+          );
+        })}
+      </div>
+      {selectedProject && (
+        <div className='overlay'>
+          <div className='modal'>
+            <h4 className='over-title'>{selectedProject.ptitle}</h4>
+            <p className='my-tech'>Tech Stack: {selectedProject.tech}</p>
+            <br />
+            <a href={selectedProject.deploy} target='_blank' rel='noreferrer' className='deploy-class'>
+              Deployment Link
+            </a>
+            <button className='close-button' onClick={handleCloseModal}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
-export default Projects
+  );
+};
+
+export default Projects;
